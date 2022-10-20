1edd6e24e7f75b6adb3c29f15185ddeeb0729fe3
Leomara <leomara.castro@apagri.com.br>
2022-10-20 20:00:00 -0300

    usecase para checar o estoque

---
+++ src/modules/product-adm/usecase/check-stock/check-stock.usecase.spec.ts
@@ -0,0 +1,37 @@
+import Product from '../../domain/product.entity';
+import Id from '../../../@shared/domain/value-object/id.value-object';
+import CheckStockUsecase from './check-stock.usecase';
+
+const product = new Product({
+    id: new Id('1'),
+    name: 'Product 1',
+    description: 'Product 1 description',
+    purchasePrice: 10,
+    stock: 10,
+});
+
+const mockRepository = () => {
+    return {
+        add: jest.fn(),
+        find: jest.fn().mockReturnValue(product)
+    };
+};
+
+describe('Check stock use case unit test', () => {
+    it('should get the stock', async () => {
+        const repository = mockRepository();
+        const usecase = new CheckStockUsecase(repository);
+        const output = await usecase.execute({productId: product.id.id});
+        expect(repository.find).toBeCalled();
+        expect(output.stock).toBe(product.stock);
+        expect(output.productId).toBe(product.id.id);
+    });
+
+    it('should throw error when not found product', () => {
+        const errorMessage = `Product ${product.id.id} not found`;
+        const repository = mockRepository();
+        repository.find.mockRejectedValue(new Error(errorMessage));
+        const usecase = new CheckStockUsecase(repository);
+        expect(usecase.execute({productId: product.id.id})).rejects.toThrowError(errorMessage);
+    });
+});
---
+++ src/modules/product-adm/usecase/check-stock/check-stock.usecase.ts
@@ -0,0 +1,20 @@
+import UseCaseInterface from '../../../@shared/usecase/use-case.interface';
+import ProductGateway from '../../gateway/product.gateway';
+import {CheckStockInputDto, CheckStockOutputDto} from './check-stock.dto';
+
+export default class CheckStockUsecase implements UseCaseInterface<CheckStockInputDto, CheckStockOutputDto> {
+    private _repository: ProductGateway;
+
+    constructor(repository: ProductGateway) {
+        this._repository = repository;
+    }
+
+    async execute(input: CheckStockInputDto): Promise<CheckStockOutputDto> {
+        const product = await this._repository.find(input.productId);
+        return {
+            productId: product.id.id,
+            stock: product.stock
+        };
+    }
+
+}
