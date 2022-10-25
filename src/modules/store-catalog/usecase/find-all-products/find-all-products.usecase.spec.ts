893fbb972422b93a960fb363e145199b966116e6
Leomara <leomara.castro@apagri.com.br>
2022-10-25 07:27:31 -0300

    Use case para find all products

---
+++ src/modules/store-catalog/usecase/find-all-products/find-all-products.usecase.spec.ts
@@ -0,0 +1,32 @@
+import Product from '../../domain/product.entity';
+import Id from '../../../@shared/domain/value-object/id.value-object';
+import FindAllProductsUsecase from './find-all-products.usecase';
+
+const product1 = new Product({id: new Id('1'), name: 'P1', description: 'PD1', salesPrice: 100});
+const product2 = new Product({id: new Id('2'), name: 'P2', description: 'PD2', salesPrice: 200});
+const mockRepository = () => {
+    return {
+        findAll: jest.fn().mockReturnValue(Promise.resolve([product1, product2])),
+        find: jest.fn()
+    };
+
+};
+
+describe('Find all products unit test', () => {
+    it('should find all products', async () => {
+        const repository = mockRepository();
+        const usecase = new FindAllProductsUsecase(repository);
+        const result = await usecase.execute();
+
+        expect(repository.findAll).toHaveBeenCalled();
+        expect(result.products.length).toBe(2);
+        expect(result.products[0].id).toBe(product1.id.id);
+        expect(result.products[0].name).toBe(product1.name);
+        expect(result.products[0].description).toBe(product1.description);
+        expect(result.products[0].salesPrice).toBe(product1.salesPrice);
+        expect(result.products[1].id).toBe(product2.id.id);
+        expect(result.products[1].name).toBe(product2.name);
+        expect(result.products[1].description).toBe(product2.description);
+        expect(result.products[1].salesPrice).toBe(product2.salesPrice);
+    });
+});
---
+++ src/modules/store-catalog/usecase/find-all-products/find-all-products.usecase.ts
@@ -0,0 +1,22 @@
+import UseCaseInterface from '../../../@shared/usecase/use-case.interface';
+import {FindAllProductsOutputDto} from './find-all-products.dto';
+import ProductGateway from '../../gateway/product.gateway';
+
+export default class FindAllProductsUsecase implements UseCaseInterface<any, FindAllProductsOutputDto> {
+
+    constructor(private _productRepository: ProductGateway) {
+    }
+
+    async execute(): Promise<FindAllProductsOutputDto> {
+        const products = await this._productRepository.findAll();
+        return {
+            products: products.map(product => ({
+                id: product.id.id,
+                name: product.name,
+                description: product.description,
+                salesPrice: product.salesPrice
+            }))
+        };
+    }
+
+}
