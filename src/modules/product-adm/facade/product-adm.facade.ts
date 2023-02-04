51ce2e31f4d369f158fd6227f252df422368cd52
Leomara <leomara.castro@apagri.com.br>
2023-02-04 07:31:18 -0300

    removendo import

--- src/modules/product-adm/facade/product-adm.facade.ts
+++ src/modules/product-adm/facade/product-adm.facade.ts
@@ -2,8 +2,6 @@ import ProductAdmFacadeInterface from './product-adm.facade.interface';
 import {AddProductFacadeInputDto} from './dto/add-product.facade.dto';
 import {CheckStockFacadeInputDto, CheckStockFacadeOutputDto} from './dto/check-stock.facade.dto';
 import UseCaseInterface from '../../@shared/usecase/use-case.interface';
-import {AddProductInputDto, AddProductOutputDto} from '../usecase/add-product/add-product.dto';
-import {CheckStockInputDto, CheckStockOutputDto} from '../usecase/check-stock/check-stock.dto';
 
 export interface UseCasesProps {
     addUseCase: UseCaseInterface;
